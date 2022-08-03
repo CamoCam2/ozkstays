bed4c145266abdc1bb478f3cb3100d0591157134
Cam Paschke <cpaschke13@gmail.com>
2022-08-03 15:32:59 -0500

    Updating angular versions, adding google analytics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,12 @@ import {IonicModule, IonicRouteStrategy} from '@ionic/angular';
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app-routing.module';
 import {HttpClientModule} from '@angular/common/http';
+import {NgxGoogleAnalyticsModule} from 'ngx-google-analytics';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule ],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, NgxGoogleAnalyticsModule.forRoot('G-9ZR6X0P79F') ],
   providers: [{provide: RouteReuseStrategy, useClass: IonicRouteStrategy}],
   bootstrap: [AppComponent],
 })
